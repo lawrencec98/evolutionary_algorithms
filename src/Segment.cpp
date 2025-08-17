4691f65a16af76f8f490b2fd56131fe3828bbe38
lawrencec98 <lawrencemcatapang@gmail.com>
2025-08-17 10:13:25 +0100

    Add default initialization to segment vertices

--- src/Segment.cpp
+++ src/Segment.cpp
@@ -4,9 +4,16 @@
 
 #define IMAGE_WIDTH 1280
 #define IMAGE_HEIGHT 720
+#define SEG_NUM_VERTICES 100
 
 
-Segment::Segment() {
+Segment::Segment() 
+    :m_vertices(SEG_NUM_VERTICES) {
+
+    if (m_vertices.size() != SEG_NUM_VERTICES ) {
+        std::cerr << "Error, this segment has invalid number of vertices " << m_vertices.size() << std::endl;
+    }
+
     // randomly generate your vector of vertices
     for (int i = 0; i < m_vertices.size(); ++i) {
         int randomNumberX = rand() % IMAGE_WIDTH;
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
-// evolutionary algorithm will optimize image segmentation accuracy.
+// Evolutionary algorithm will optimize image segmentation accuracy.
 
-// starting population will be 100 randomly generated segments.
+// Starting population will be 100 randomly generated Segments.
 
-// fitness function will be intersection over union of those segments with the ground truth segment.
+// Fitness function will be intersection over union of those segments with the ground truth segment.
 
 
 #include "Segment.hpp"
@@ -11,7 +11,14 @@
 
 
 int main() {
+    std::cout << "Start of program...\n";
 
-    // std::vector<Segment> population(100);
-    std::cout << "Hello world\n";
+    // Start the program with 100 randomly constructed Segments.
+    std::vector<Segment> population(100);
+
+    // Run the fitness function on the population.
+    // Kill the bottom 50 Segments.
+    // Reproduce + Mutations.
+    // Higher fitness population has higher chance to reproduce.
+
 }
